54b9cf76bed649b465dd0d6b3811c0054e398f58
Alex Miller <apmiller108@yahoo.com>
2022-03-27 10:34:11 -0500

    Extracts copy script

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# Copies the mapping files to Mixxx's user controller directory and starts Mixxx
-# with controller debugging.
-cp Pioneer-DDJ-400* ~/.mixxx/controllers && pasuspender -- mixxx --controllerDebug
+# Copies the mapping files to Mixxx's user controller directory
+cp Pioneer-DDJ-400* ~/.mixxx/controllers
+
